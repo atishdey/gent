993ee29584ad2fbbb07e2b899afac82e0c4cf438
Atish Dey <atdey@deloitte.com>
2024-04-15 14:04:53 +0530

    Merged

---
+++ src/main/java/com/foliole/gent/GentApplication.java
@@ -0,0 +1,13 @@
+package com.foliole.gent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foliole/gent/GentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foliole.gent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
